b3e3b2bcd6672fa3716cad7e7f8d31f188c709c5
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-08-19 17:58:16 -0400

    with progressbars

--- combo_and_spinbox.py
+++ combo_and_spinbox.py
@@ -41,7 +41,29 @@ year = StringVar()
 
 Spinbox(root, from_ = 1990, to = 2017, textvariable = year).pack()
 
+progressbar = ttk.Progressbar(root, orient=HORIZONTAL, length = 200)
+progressbar.pack()
+progressbar.config(mode = 'indeterminate')
+progressbar.start()
 
+progressbar2 = ttk.Progressbar(root, orient=HORIZONTAL, length = 200)
+progressbar2.config(mode='determinate')
+progressbar2.start()
+progressbar2.pack()
+
+progressbar3 = ttk.Progressbar(root, orient=HORIZONTAL, length=200)
+progressbar3.config(value=8.0)
+progressbar3.step(5)
+progressbar3.config(mode='determinate', maximum = 11.0, value = 4.2)
+
+progressbar3.pack()
+
+value = DoubleVar()
+
+progressbar3.config(variable = value)
+
+scale = ttk.Scale(root, orient=HORIZONTAL, length=400, variable=value, from_ = 0.0, to = 11)
+scale.pack()
 
 
 
